07ff04e6ce2fd5b1c6d05f4e7ac21c5c65fd5bb4
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-02-05 01:19:18 +0100

    Added tests to cover column validation. Fixed bugs discovered during tests.

--- colanderalchemy/__init__.py
+++ colanderalchemy/__init__.py
@@ -6,14 +6,19 @@ import sqlalchemy.orm
 import sqlalchemy.orm.properties
 import sqlalchemy.types
 
-def get_schema_from_column(property_):
+def get_schema_from_column(property_, registry):
     """ Build and return a Colander SchemaNode
         using information stored in the column property.
     """
 
     column = property_.columns[0]
     validator = None
-    missing = None
+    missing = colander.required
+
+    if column.primary_key:
+        registry['id'] = property_.key
+
+    registry['fields'].append(property_.key)
 
     if isinstance(column.type, sqlalchemy.types.Boolean):
 
@@ -30,7 +35,7 @@ def get_schema_from_column(property_):
     elif isinstance(column.type, sqlalchemy.types.Enum):
 
         type_ = colander.String()
-        # FIXME: add validator = ONEOF()
+        validator = colander.OneOf(column.type.enums)
 
     elif isinstance(column.type, sqlalchemy.types.Float):
 
@@ -57,11 +62,11 @@ def get_schema_from_column(property_):
         raise NotImplemented('Unknown type: %s' % column.type)
 
     # Add a default value for deserialized missing parameters.
-    if missing is None and \
-      (column.nullable or not column.default is None):
-        missing = column.default
-    elif missing is None:
-        missing = colander.required
+    if not column.default is None:
+        missing = column.default.arg
+
+    elif column.nullable:
+        missing = None
 
     return colander.SchemaNode(type_,
                                name=property_.key,
@@ -69,6 +74,7 @@ def get_schema_from_column(property_):
                                missing=missing)
 
 def get_schema_from_relationship(property_,
+                                 registry,
                                  get_schema_from_column=get_schema_from_column):
     """ Build and return a Colander SchemaNode
         using information stored in the relationship property.
@@ -83,17 +89,20 @@ def get_schema_from_relationship(property_,
     mapper = sqlalchemy.orm.class_mapper(cls)
     prop = mapper.get_property_by_column(mapper.primary_key[0])
 
-    node = colander.SchemaNode(colander.Mapping(),
-                               get_schema_from_column(prop))
+    node = get_schema_from_column(prop)
 
     if property_.uselist:
+        # xToMany relationships.
         node = colander.SchemaNode(colander.Sequence(),
                                    node,
                                    name=property_.key,
                                    missing=[])
+        registry['relationships']['many'][property_.key] = cls
     else:
+        # xToOne relationships.
         node.name = property_.key
         node.missing = None
+        registry['relationships']['one'][property_.key] = cls
 
     return node
 
@@ -101,8 +110,17 @@ def get_schema(entity,
                get_schema_from_column=get_schema_from_column,
                get_schema_from_relationship=get_schema_from_relationship):
 
+    # An entity's schema has the following structure:
+    # {'attr1': value1, 'attr2': value2, ...}
     schema = colander.SchemaNode(colander.Mapping())
-    registry = {}
+    registry = {
+        'id': None, # The name of the field used to identify the entity.
+        'fields': [],
+        'relationships': {
+            'one': {},
+            'many': {},
+        },
+    }
 
     mapper = sqlalchemy.orm.class_mapper(entity)
 
@@ -113,17 +131,14 @@ def get_schema(entity,
 
         if isinstance(prop, sqlalchemy.orm.properties.ColumnProperty):
 
-            node = get_schema_from_column(prop)
-
-            if prop.columns[0].primary_key:
-                registry['id'] = prop.key
+            node = get_schema_from_column(prop, registry)
 
         elif isinstance(prop, sqlalchemy.orm.properties.RelationshipProperty):
 
             node = get_schema_from_relationship(prop, get_schema_from_column)
 
         else:
-            NotImplemented('Unknown property: %s' % prop)
+            NotImplemented('Unsupported property type: %s' % prop)
 
         schema.add(node)
 
--- colanderalchemy/tests.py
+++ colanderalchemy/tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import colander
 import colanderalchemy
 import sqlalchemy
 #from sqlalchemy.orm import backref
@@ -40,8 +41,66 @@ class TestsBase(unittest.TestCase):
             name = sqlalchemy.Column(sqlalchemy.Unicode(8),
                                      primary_key=True)
 
+        class EnumColumn(StringColumnWithDefault):
+            choice = sqlalchemy.Column(sqlalchemy.Enum(u'A', u'B'))
+
         self.StringColumn = StringColumn
+        self.StringColumnNotNullable = StringColumnNotNullable
+        self.StringColumnWithDefault = StringColumnWithDefault
+        self.StringColumnAsPK = StringColumnAsPK
+        self.EnumColumn = EnumColumn
 
     def test_string_column(self):
         schema = colanderalchemy.get_schema(self.StringColumn)
-        assert colanderalchemy.get_registry(schema) != {}
+
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {})
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {'name': 'A Name.'})
+
+        self.assertEqual({'id': 1, 'name': None},
+                         schema.deserialize({'id': 1}))
+
+        registry = colanderalchemy.get_registry(schema)
+        self.assertTrue('id' in registry)
+        self.assertEqual(registry['id'], 'id')
+        self.assertTrue('fields' in registry)
+        self.assertEqual(registry['fields'], ['id', 'name'])
+
+    def test_string_column_not_nullable(self):
+
+        schema = colanderalchemy.get_schema(self.StringColumnNotNullable)
+
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {'id': 1})
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {'id': 1, 'name': None})
+
+        self.assertEqual({'id': 1, 'name': 'My Name.'},
+                         schema.deserialize({'id': 1, 'name': 'My Name.'}))
+
+    def test_string_column_with_default(self):
+
+        schema = colanderalchemy.get_schema(self.StringColumnWithDefault)
+        self.assertEqual({'id': 1, 'name': 'Default Value'},
+                         schema.deserialize({'id': 1}))
+
+    def test_string_column_as_pk(self):
+
+        schema = colanderalchemy.get_schema(self.StringColumnAsPK)
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {})
+        self.assertEqual({'name': 'My Name.'},
+                         schema.deserialize({'name': 'My Name.'}))
+
+    def test_enum_column(self):
+
+        schema = colanderalchemy.get_schema(self.EnumColumn)
+        self.assertRaises(colander.Invalid,
+                          schema.deserialize, {'id': 1, 'choice': 'C'})
+        data = {'id': 1, 'name': 'Default Value', 'choice': None}
+        self.assertEqual(data, schema.deserialize({'id': 1}))
+        data = {'id': 1, 'name': 'My Name.', 'choice': 'A'}
+        self.assertEqual(data, schema.deserialize(data))
+        data = {'id': 1, 'name': 'My Name.', 'choice': 'B'}
+        self.assertEqual(data, schema.deserialize(data))

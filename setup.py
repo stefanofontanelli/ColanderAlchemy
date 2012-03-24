c34af06d04c44f9f1b5e5727af306649e46b221d
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-03-24 11:30:50 +0100

    Configured setup for PyPi.

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.1.0a'
+version = '0.1.0'
 
 setup(name='ColanderAlchemy',
       version=version,
-      description="Create a Colander schema reading SQLAlchemy model.",
+      description="Autogenerate Colander schemas based on SQLAlchemy models.",
       long_description="""\
-A set of tools to convert SQLAlchemy models into Colander schemas.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='Colander SQLAlchemy',
+An helper tool for autogenerating Colander schemas based on SQLAlchemy mapped classes.""",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent'],
+      keywords='serialize deserialize validate schema validation colander sqlalchemy',
       author='Stefano Fontanelli',
       author_email='s.fontanelli@asidev.com',
-      url='http://code.asidev.com/colander-alchemy',
+      url='https://github.com/stefanofontanelli/ColanderAlchemy',
       license='MIT',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=True,
       install_requires=['Colander', 'SQLAlchemy'],
-      tests_require=['nose >= 0.11'],
+      tests_require=[],
       test_suite='test',
       )

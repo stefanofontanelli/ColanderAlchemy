8eebf3909f654c9ed0aa01b81aef75ee5307cfd3
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-05-19 11:19:42 +0200

    Change revision number in setup.py Remove tag from setup.cfg before release.

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
-version = '0.1.0a2dev'
+version = '0.1b'
 
 setup(name='ColanderAlchemy',
       version=version,
       description="Autogenerate Colander schemas based on SQLAlchemy models.",
       long_description="""\
 An helper tool for autogenerating Colander schemas based on SQLAlchemy mapped classes.""",
-      classifiers=['Development Status :: 3 - Alpha',
+      classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.2',
                    'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent'],
+                   'Operating System :: OS Independent',
+                   'Topic :: Database'],
       keywords='serialize deserialize validate schema validation colander sqlalchemy',
       author='Stefano Fontanelli',
       author_email='s.fontanelli@asidev.com',

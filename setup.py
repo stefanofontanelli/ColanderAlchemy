f9414b6784290218a4364342a8b0c4e702496a20
Stefano Fontanelli <s.fontanelli@asidev.com>
2012-10-11 16:01:36 +0200

    Increase version number 0.1 -> 0.2: this branch will break things :P Decrease development status from beta to alpha.

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
 setup(name='ColanderAlchemy',
       version=version,
       description="Autogenerate Colander schemas based on SQLAlchemy models.",
       long_description=open('README.rst', 'rb').read(),
-      classifiers=['Development Status :: 4 - Beta',
+      classifiers=['Development Status :: 4 - Alpha',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.1',
                    'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
                    'License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
                    'Topic :: Database'],

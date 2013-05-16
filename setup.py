dcef925d2e70107cd0451817d482ed44172f6e6e
Stefano Fontanelli <s.fontanelli@gmail.com>
2013-05-16 14:54:09 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.2'
+version = '0.2.0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()

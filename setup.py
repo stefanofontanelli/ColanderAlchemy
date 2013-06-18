259c5245fa1efccd7a3a19bb312bd8f66edcde17
David\ Beitey <david@davidjb.com>
2013-06-18 14:11:32 +1000

    Rename change log to CHANGES.rst for GitHub.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.2.0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGELOG.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 setup(name='ColanderAlchemy',
       version=version,
